907a120354b28aadc7548f3e514fc56e5277cc22
root <stephanus.tedy@gmail.com>
2017-06-27 23:09:55 +0700

    move env

--- src/database/init.go
+++ src/database/init.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"log"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,4 +13,12 @@ func Initialize() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	result, err := db.Exec(
+ "create table IF NOT EXISTS employee (employeeID integer PRIMARY KEY,name varchar(255) NOT null,age int, person_id int, FOREIGN KEY (person_id) REFERENCES persons(id), CONSTRAINT uc_empID UNIQUE (employeeID, person_id, name))",)
+ 
+if err != nil {
+ log.Fatal(err)
+}
+log.Println(result)
 }
