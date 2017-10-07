40830f56597b96b259ccb6cdf251592f5ff3f102
stephanustedy <stephanus.tedy@gmail.com>
2017-10-07 23:42:03 +0700

    update gitignore

---
+++ src/myreviewer/constants.go
@@ -0,0 +1,10 @@
+package myreviewer
+
+const (
+	REVIEWER_STATUS_NOT_APPROVE = "-1"
+	REVIEWER_STATUS_APPROVE     = "2"
+	REVIEWER_STATUS_BUSY        = "-2"
+	REVIEWER_STATUS_ACTIVE      = "1"
+
+	REVIEW_STATUS_DONE = "2"
+)
